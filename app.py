309692c79ab2ef5d129568529286fa8a7dedf39c
sudhakaranjain <sudhakaran.cj@gmail.com>
2020-09-13 22:29:53 +0530

    changed model to pre-trained MobileNet

--- app.py
+++ app.py
@@ -1,23 +1,21 @@
 import numpy as np
 import os
+import json
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 from flask import Flask, request, jsonify, render_template
 import pickle
-# import urllib.request
 import cv2 as cv
 import tensorflow as tf
 from werkzeug.utils import secure_filename
 
+IMG_SIZE = 224
+IMG_SHAPE = (IMG_SIZE, IMG_SIZE, 3)
+
 global graph
 graph = tf.get_default_graph()
-classes = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+# classes = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
 app = Flask(__name__)
-# model = pickle.load(open('model.pkl', 'rb'))
-UPLOAD_FOLDER = 'static/uploads/'
-
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 @app.route('/')
 def home():
@@ -28,10 +26,9 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-    # int_features = [int(x) for x in request.form.values()]
+
     image = request.files['image']
     filename = secure_filename(image.filename)
-    # image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
     image.stream.seek(0) # seek to the beginning of file
     image_byte = image.read()
@@ -40,18 +37,24 @@ def predict():
     # cv.imshow('', file)
     # cv.waitKey(0)
     # cv.destroyAllWindows()
-    file = cv.resize(file, (32,32))
+    file = cv.resize(file, (IMG_SIZE, IMG_SIZE))
     file = np.array([file]) / 255.0
-    # print(file.shape)
-    # keras.backend.clear_session()
-    with graph.as_default():
-        model = tf.keras.models.load_model('./model.h5')
-        prediction = model.predict(file, batch_size=file.shape[0])
-    output = np.argmax(prediction, axis=1)
-    class_name = classes[output[0]]
-    # output = round(prediction[0], 2)
+    # file = np.array([file])
 
-    return render_template('index.html', prediction_text='The image is a {}'.format(class_name))
+    with graph.as_default():
+        mobilenet = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE, weights="imagenet")
+        mobilenet.trainable = False
+        prediction = mobilenet.predict(file, batch_size=file.shape[0])
+
+    top_5_classes_index = np.argsort(prediction)[0 , ::-1][:5]+1
+
+    labels_path = tf.keras.utils.get_file('ImageNetLabels.txt',
+        'https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt')
+    labels = np.array(open(labels_path).read().splitlines())
+    top_5_classes = labels[top_5_classes_index]
+    class_name = ", ".join(top_5_classes)
+    # return render_template('index.html', prediction_text='The image is a {}'.format(class_name))
+    return render_template('index.html', prediction_text='The image can be: '+class_name)
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
